2c834da75ac27bbc4a8a40c417a21446abce29cf
taka01150810 <taka01150810@gmail.com>
2022-11-12 13:00:42 +0900

    Inertia、Vue3、script setup (compositionAPI)(他のコンポーネントを使ってみる)

---
+++ uCRM/resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Label from '@/components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+</script>
+
+<template>
+<GuestLayout>
+    <Label>タイトル</Label>
+    <Input modelValue="初期値です"></Input>
+</GuestLayout>
+</template>
--- uCRM/routes/web.php
+++ uCRM/routes/web.php
@@ -20,6 +20,10 @@ Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');
 });
 
+Route::get('/component-test', function () {
+    return Inertia::render('ComponentTest');
+});
+
 Route::get('/inertia/index', [InertiaTestController::class,'index'])->name('inertia.index'); 
 Route::get('/inertia/create', [InertiaTestController::class,'create'])->name('inertia.create');
 Route::post('/inertia', [InertiaTestController::class,'store'])->name('inertia.store'); 
