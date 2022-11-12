fb7bb59832802d9ee771377812d9968a3cdc8898
taka01150810 <taka01150810@gmail.com>
2022-11-12 12:06:49 +0900

    Inertia、Vue3、script setup (compositionAPI)(バリデーション)

--- uCRM/lang/ja/validation.php
+++ uCRM/lang/ja/validation.php
@@ -153,6 +153,9 @@
      |
      */
  
-     'attributes' => [],
+     'attributes' => [
+        'title' => '件名',
+        'content' => '本文',
+     ],
  
  ];
--- uCRM/resources/js/Pages/Inertia/Create.vue
+++ uCRM/resources/js/Pages/Inertia/Create.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { reactive } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
+
+defineProps({
+ errors: Object
+})
+
 const form = reactive({
     title: null,
     content: null
@@ -14,7 +19,9 @@ const submitFunction = () => {
 <template>
 <form @submit.prevent="submitFunction">
     <input type="text" name="title" v-model="form.title">
+    <div v-if="errors.title">{{ errors.title }}</div>
     <input type="text" name="content" v-model="form.content">
+    <div v-if="errors.content">{{ errors.content }}</div> 
     <button>送信</button>
 </form>
 </template>
