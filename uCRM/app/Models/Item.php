668e37b54a87c2847d5c0b454dfb9339f399bcaf
taka01150810 <taka01150810@gmail.com>
2022-11-13 11:54:22 +0900

    CRM 商品関連(Items モデル・ルーティング)

--- uCRM/app/Models/Item.php
+++ uCRM/app/Models/Item.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    // コントローラ側にて Item::create() で保存できるようにモデル側に追記
+    protected $fillable = [
+        'name',
+        'memo',
+        'price',
+        'is_selling'
+    ];
 }
