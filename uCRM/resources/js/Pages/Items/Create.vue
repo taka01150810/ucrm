2524d59bc166b5d9d2c6c7b405185c0755ec6acd
taka01150810 <taka01150810@gmail.com>
2022-11-13 13:14:03 +0900

    CRM 商品関連(Items store)

--- uCRM/resources/js/Pages/Items/Create.vue
+++ uCRM/resources/js/Pages/Items/Create.vue
@@ -54,7 +54,7 @@ const storeItem = () => {
                                             <div class="p-2 w-full">
                                             <div class="relative">
                                                 <label for="price" class="leading-7 text-sm text-gray-600">値段</label>
-                                                <input type="text" id="price" name="price" v-model="form.name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                <input type="text" id="price" name="price" v-model="form.price" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                             </div>
                                             </div>
                                             <div class="p-2 w-full">
