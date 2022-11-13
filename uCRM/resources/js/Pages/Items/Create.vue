cc7596881870e8d90bb4f95a008f62cd1827927f
taka01150810 <taka01150810@gmail.com>
2022-11-13 13:33:09 +0900

    CRM 商品関連(Items バリデーション)

--- uCRM/resources/js/Pages/Items/Create.vue
+++ uCRM/resources/js/Pages/Items/Create.vue
@@ -3,6 +3,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 import { reactive } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
 
 defineProps({
  errors: Object
@@ -34,6 +35,7 @@ const storeItem = () => {
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white border-b border-gray-200">
+                            <BreezeValidationErrors class="mb-4" />
                             <section class="text-gray-600 body-font relative">
                                 <form @submit.prevent="storeItem">
                                     <div class="container px-5 py-8 mx-auto">
