a223d5ac6e0d2f5ea13e20f630fe6e5dc7e94852
taka01150810 <taka01150810@gmail.com>
2023-02-02 23:24:53 +0900

    CRM 購入画面(合計金額をcomputedで計算)

--- uCRM/resources/js/Pages/Purchases/Create.vue
+++ uCRM/resources/js/Pages/Purchases/Create.vue
@@ -2,6 +2,7 @@
 import { getToday } from '@/common' // 別ファイルをインポート
 import { onMounted, reactive } from 'vue'
 import { ref } from 'vue'
+import { computed } from 'vue'
 
 // ページ読み込み後 即座に実行
 onMounted(() => {
@@ -34,6 +35,14 @@ onMounted(() => {
 
 // option用
 const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+const totalPrice = computed(() => {
+    let total = 0
+    itemList.value.forEach( item => {
+        total += item.price * item.quantity
+    })
+    return total
+})
 </script> 
 
 <template>
@@ -67,5 +76,7 @@ const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
             </td>
             <td>{{ item.price * item.quantity }}</td>
         </tr>
-    </tbody> 
+    </tbody>
+    <br>
+    合計 {{ totalPrice }} 円
 </template>
