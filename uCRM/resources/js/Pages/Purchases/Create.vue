3fcbb37eb2e10cc48b612a66d4688bf845b9cde7
taka01150810 <taka01150810@gmail.com>
2023-02-02 23:21:42 +0900

    CRM 購入画面(商品情報をv-forで表示)

--- uCRM/resources/js/Pages/Purchases/Create.vue
+++ uCRM/resources/js/Pages/Purchases/Create.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { getToday } from '@/common' // 別ファイルをインポート
 import { onMounted, reactive } from 'vue'
+import { ref } from 'vue'
 
 // ページ読み込み後 即座に実行
 onMounted(() => {
@@ -8,7 +9,8 @@ onMounted(() => {
 })
 
 const props = defineProps({
-    'customers': Array
+    'customers': Array,
+    'items': Array
 })
 
 const form = reactive({
@@ -16,6 +18,22 @@ const form = reactive({
     customer_id: null
 })
 
+// リアクティブな配列を準備
+const itemList = ref([])
+
+onMounted(() => {
+    props.items.forEach( item => { // 配列を1つずつ処理
+        itemList.value.push({ // 配列に1つずつ追加
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            quantity: 0
+        })
+    })
+})
+
+// option用
+const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 </script> 
 
 <template>
@@ -28,6 +46,26 @@ const form = reactive({
         {{ customer.id }} : {{ customer.name }}
     </option>
     </select>
-
-    
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>商品名</th>
+            <th>金額</th>
+            <th>数量</th>
+            <th>小計</th>
+        </tr>
+    </table>
+    <tbody>
+        <tr v-for="item in itemList" :key="item">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.price }}</td>
+            <td>
+                <select name="quantity" v-model="item.quantity">
+                    <option v-for="q in quantity" :value="q" :key="q">{{ q }}</option>
+                </select>
+            </td>
+            <td>{{ item.price * item.quantity }}</td>
+        </tr>
+    </tbody> 
 </template>
