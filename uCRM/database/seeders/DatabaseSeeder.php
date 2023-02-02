146a12bdb3160bc8132593d8293e56c6727b01b0
taka01150810 <taka01150810@gmail.com>
2023-02-02 22:38:41 +0900

    CRM 購入画面(中間テーブルのダミーデータ作成)

--- uCRM/database/seeders/DatabaseSeeder.php
+++ uCRM/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Purchase;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,5 +23,24 @@ class DatabaseSeeder extends Seeder
         \App\Models\Customer::factory(1000)->create();
 
         \App\Models\Purchase::factory(100)->create();
+        
+        /*
+        Purchaseを登録時 中間テーブルにも同時に登録する(1件の購入情報に1件～3件の商品情報を登録とする)
+        each・・・1件ずつ処理
+        attach・・・中間テーブルに情報登録
+        外部キー以外で中間テーブルに情報追加するには第2引数に書く 
+        */
+        
+        $items = \App\Models\Item::all();
+        
+        // use($items) ... 関数の外側の変数を使うことができる
+        Purchase::factory(100)->create()
+        ->each(function(Purchase $purchase) use ($items){
+            $purchase->items()->attach(
+                $items->random(rand(1,3))->pluck('id')->toArray(),
+                // 1～3個のitemをpurchaseにランダムに紐づけ
+                ['quantity' => rand(1, 5) ]
+            );
+        });
     }
 }
