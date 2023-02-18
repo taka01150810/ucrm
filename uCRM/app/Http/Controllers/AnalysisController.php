c5b0c34caddc351dea42e63f51a80ffc3d8f6b47
taka01150810 <taka01150810@gmail.com>
2023-02-18 14:12:26 +0900

    データ分析(日別売上のクエリ作成・取得確認)

--- uCRM/app/Http/Controllers/AnalysisController.php
+++ uCRM/app/Http/Controllers/AnalysisController.php
@@ -5,25 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
     public function index()
     {
+        /*
+        1. 購買id毎の売上をまとめ, dateをフォーマットした状態のサブクエリをつくる
+        2. サブクエリをgroupByで日毎にまとめる 
+        */
+        // 期間指定
         $startDate = '2022-08-01';
-        $endDate = '2022-08-10';
+        $endDate = '2022-08-31';
         
-        $period = Order::betweenDate($startDate, $endDate)
+        // 日別
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->where('status', true)
         ->groupBy('id')
         ->selectRaw(
             'id,
-            sum(subtotal) as total,
-            customer_name,
-            status,
-            created_at'
-        )
-        ->orderBy('created_at')
-        ->paginate(50);
+            SUM(subtotal) as totalPerPurchase,
+            DATE_FORMAT(created_at, "%Y%m%d") as date'
+        );
+        
+        $data = DB::table($subQuery)
+        ->groupBy('date')
+        ->selectRaw('date, sum(totalPerPurchase) as total')
+        ->get();
+        
+        // dd($data);
 
         return Inertia::render('Analysis');
     } 
--- uCRM/app/Http/Controllers/Api/AnalysisController.php
+++ uCRM/app/Http/Controllers/Api/AnalysisController.php
@@ -3,17 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Models\Order;
+use Illuminate\Support\Facades\DB; 
 
 class AnalysisController extends Controller
 {
     public function index(Request $request)
     {
+        $subQuery = Order::betweenDate($request->startDate, $request->endDate);
+
+        if($request->type === 'perDay'){
+            $subQuery
+            ->where('status', true)
+            ->groupBy('id')
+            ->selectRaw(
+                'SUM(subtotal) AS totalPerPurchase,
+                DATE_FORMAT(created_at, "%Y%m%d") AS date'
+            )
+            ->groupBy('date');
+            
+            $data = DB::table($subQuery)
+            ->groupBy('date')
+            ->selectRaw('date, sum(totalPerPurchase) as total')
+            ->get();
+        }
+        
+        
         // Ajax通信なのでJsonで返却する必要がある
         return response()->json([
-            'data' => $request->startDate // 仮設定
+            'data' => $data,
+            'type' => $request->type
         ], Response::HTTP_OK);
     } 
 }
