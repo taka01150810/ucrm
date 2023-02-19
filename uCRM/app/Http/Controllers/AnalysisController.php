172a913e6b78c6378c18d46632ff447c77dcb694
taka01150810 <taka01150810@gmail.com>
2023-02-19 11:07:37 +0900

    データ分析(RFM分析(RFMそれぞれの情報を取得))

--- uCRM/app/Http/Controllers/AnalysisController.php
+++ uCRM/app/Http/Controllers/AnalysisController.php
@@ -12,7 +12,7 @@ class AnalysisController extends Controller
     public function index()
     {
         // 期間指定
-        $startDate = '2022-08-01';
+        $startDate = '2021-08-01';
         $endDate = '2022-08-31';
 
         // 1. 購買ID毎にまとめる
@@ -100,6 +100,34 @@ class AnalysisController extends Controller
         ->get();
 
         // dd($data);
+
+        // 1. 購買ID毎にまとめる
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->groupBy('id')
+        ->selectRaw('
+            id,
+            customer_id,
+            customer_name,
+            SUM(subtotal) as totalPerPurchase,
+            created_at
+        ');
+
+        // datediffで日付の差分, maxで日付の最新日
+        // 2. 会員毎にまとめて最終購入日、回数、合計金額を取得
+        $subQuery = DB::table($subQuery)
+        ->groupBy('customer_id')
+        ->selectRaw('
+            customer_id, 
+            customer_name,
+            max(created_at) as recentDate, 
+            datediff(now(), 
+            max(created_at)) as recency,
+            count(customer_id) as frequency,
+            sum(totalPerPurchase) as monetary
+        ')
+        ->get();
+
+        dd($subQuery);
 
         return Inertia::render('Analysis');
     } 
